37bb04ee6a2b6a203c30197cf549aa5b0c9810be
aomaeming <auysmilelove@gmail.com>
2026-08-31 12:58:21 +0700

    Add Lab03

---
+++ Lab03/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Student ID : 1690700404
+ * Name       : Sukruethai Noppakao
+ * Section    : 129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+namespace Lab03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            // output แสดงค่าตัวแปร
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName}\nRank: {rank}\nLevel: {level}\nHP: {currentHp} / {maxHp} +
+                $"\nAttack Power: {attackPower}\nCritical Multiplier: {critMultiplier}\nIs Boss: {isBoss}");
+
+            // ทำ implicit int -> double
+            Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+            double currentHpDouble = currentHp; // int ->> double แปลงแบบไม่ cast ใส่ค่าได้เลย
+            Console.WriteLine($"HP (double): {currentHpDouble}");
+
+            // ทำ implicit หลังการคำนวณเปอร์เซ็นต์ ที่ผลลัพธ์มีทศนิยม
+            Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+            double hpPercentExact = currentHpDouble * 100 / maxHp; // int หาร int จากนั้นแปลงผลลัพธ์การคำนวณเป็น double เพื่อเก็บทศนิยม
+            Console.WriteLine($"HP Percent (exact): {hpPercentExact}%");
+
+            // ทำ explicit โดยการ caasting (float) attackpower ->> int
+            Console.WriteLine("\n----- Explicit Cast: Attack Power -> Display Int -----");
+            int attackDisplay = (int)attackPower; // แปลงข้อมูลโดยการ cast syntax: (ชนิดที่ต้องการแปลง)ชื่อตัวแปรที่อยากแปลง
+            Console.WriteLine($"Attack Power (int cast): {attackDisplay}");
+
+            // ส่วนสุดท้ายทำเอง cast + convert
+            Console.WriteLine("\n----- Cast vs Convert: Crit Multiplier -----");
+            int critCast = (int)critMultiplier;
+            int critConvert = Convert.ToInt32(critMultiplier);
+            Console.WriteLine($"Crit Multiplier (cast): {critCast}\nCrit Multiplier (convert): {critConvert}");
+
+        }
+    }
+}
